2760bf18b1b45d127710d7ee382bb9b52dc260ec
ola <kveola13@student.westerdals.no>
2020-02-12 17:51:18 +0100

    Init

---
+++ src/main/java/com/kveola13/hibernation/App.java
@@ -0,0 +1,13 @@
+package com.kveola13.hibernation;
+
+
+public class App {
+    public static void main(String[] args) {
+        Alien person = new Alien();
+        person.setId(101);
+        person.setName("Francis France");
+        person.setName("French");
+
+
+    }
+}
